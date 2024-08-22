9daf1fd204f0bd63031ec8e6cf0c6768f5ab4de7
MICAEL FARJALLA GRANGEIRO ABUD <micael.abud@a.ucb.br>
2024-08-22 20:48:34 -0300

    n1-equipe

---
+++ Algoritimos-ProgramacaoEstruturada/n1Equipe.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+	
+	char user[5] = "adm";
+	int senha = 123;
+	
+	char userVer[5];
+	int senhaVer;
+	
+	int ok;
+
+	printf("escreva o usuario: ");
+	gets(userVer);
+			
+	printf("escreva a senha: ");
+	scanf("%d", &senhaVer);
+	
+	ok = strcmp(user, userVer);
+	
+	if(ok == 0 && senha == senhaVer) {
+		puts("e o micas");
+	} else {
+		while(ok != 0 || senha != senhaVer) {
+			system("cls");
+			printf("escreva o usuario: ");
+			scanf("%s", &userVer);
+			fflush(stdin);
+			
+			printf("escreva a senha: s");
+			scanf("%d", &senhaVer);
+	
+			ok = strcmp(user, userVer);
+			system("cls");
+		}
+	}
+}
