b36a79fd2353329b6377d19957b673fd26693029
micas <micaelabud21@gmail.com>
2024-08-29 19:03:31 -0300

    trabalho equipe feito

--- Algoritimos-ProgramacaoEstruturada/n1Equipe.c
+++ Algoritimos-ProgramacaoEstruturada/n1Equipe.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main() {
+	setlocale(LC_ALL, "Portuguese");
 	
 	char user[5] = "adm";
 	int senha = 123;
@@ -12,7 +14,7 @@ int main() {
 	
 	int ok;
 
-	printf("escreva o usuario: ");
+	printf("escreva o usuário: ");
 	gets(userVer);
 			
 	printf("escreva a senha: ");
@@ -21,15 +23,16 @@ int main() {
 	ok = strcmp(user, userVer);
 	
 	if(ok == 0 && senha == senhaVer) {
+		system("cls");
 		numeroMaior();
 	} else {
 		while(ok != 0 || senha != senhaVer) {
 			system("cls");
-			printf("escreva o usuario: ");
+			printf("escreva o usuário: ");
 			scanf("%s", &userVer);
 			fflush(stdin);
 			
-			printf("escreva a senha: s");
+			printf("escreva a senha: ");
 			scanf("%d", &senhaVer);
 	
 			ok = strcmp(user, userVer);
@@ -43,16 +46,18 @@ int main() {
 
 int numeroMaior() {
 	int a, b, c;
-	printf("Digite tres numeros apertando ENTER: \n");
-	scanf("%i %i %i", &a, %b, %c);
+	printf("Digite três números apertando ENTER: \n");
+	scanf("%i %i %i", &a, &b, &c);
+
+	system("cls");
 
 	if( (a>b) && (a>c) ) {
-		printf("\nMaior valor e o A: %d", a);
+		printf("Maior valor é o A: %d", a);
 	} else if( (b>a) && (b>c) ) {
-		printf("\nMaior valor e o B: %d", b);
+		printf("Maior valor é o B: %d", b);
 	} else if( (c>a) && (c>b) ) {
-		printf("\nMaior valor e o C: %d", c);
+		printf("Maior valor é o C: %d", c);
 	} else {
-		printf("\nDois ou mais numeros com valores igual: \nA=%d \nB=%d \nC=%d", a, b,c);
+		printf("Dois ou mais números com valores igual: \nA=%d \nB=%d \nC=%d", a, b,c);
 	}
 }
