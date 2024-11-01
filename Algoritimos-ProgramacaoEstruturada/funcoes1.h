b42daa72f586ac6dde43fd924b7c7112189bbfc3
MICAEL FARJALLA GRANGEIRO ABUD <micael.abud@a.ucb.br>
2024-10-31 21:00:21 -0300

    n2-solo

--- Algoritimos-ProgramacaoEstruturada/funcoes1.h
+++ Algoritimos-ProgramacaoEstruturada/funcoes1.h
@@ -7,6 +7,8 @@ int tosaOption[10];
 int dataOption[10];
 int medOpiton[10];
 
+int i;
+
 
 int nome_do_petshop();
 
--- Algoritimos-ProgramacaoEstruturada/n2.c
+++ Algoritimos-ProgramacaoEstruturada/n2.c
@@ -6,6 +6,15 @@
 char nome[100];
 int telefone;
 char endereco[100];
+int option;
+
+char cor[20];
+char pagamento[50];
+int optionCar;
+char carName[50];
+
+char nomeAcessorio[100];
+int quantidade;
 
 int login() {
 	int senha = 123;
@@ -41,38 +50,145 @@ int login() {
 	}
 }
 
+int menu() {
+	system("cls");
+
+	puts("1 - Cadastrar");
+	puts("2 - Comprar carro");
+	puts("3 - Comprar acessório");
+	puts("4 - Sair\n");
+	puts("escolha uma opção:");
+	
+	fflush(stdin);
+	scanf("%d", &option);
+	fflush(stdin);
+	
+	switch (option) {
+		case 1:
+			cadastro();
+			break;
+		
+		case 2: 
+			compraCarro();
+			break;
+			
+		case 3: 
+			compraAcessorio();
+			break;
+			
+		case 4: 
+			sair();
+			break;
+			
+		default:
+			system("cls");
+			puts("numero inválido");
+			system("pause");
+			menu();
+			break;
+	}
+}
+
 int cadastro() {
 	system("cls");
 	
-	puts("escreva seu nome completo:\n");
+	puts("escreva seu nome completo:");
 	gets(nome);
 	
-	puts("digite seu telefone:\n");
+	puts("digite seu telefone:");
 	scanf("%d", &telefone);
 	
-	puts("dgite seu endereÃ§o:\n");
+	puts("digite seu endereço:");
+	fflush(stdin);
 	gets(endereco);
 	
 	system("cls");
 	
-	printf("Nome completo: %s", nome);
-	printf("Seu endereco: %s", endereco);
-	printf("Telefone: %d", telefone);
+	printf("Nome completo: %s\n", nome);
+	printf("Seu endereco: %s\n", endereco);
+	printf("Telefone: %d\n\n", telefone);
+
+	system("pause");
+
+	menu();
 }
 
-/*int compraCarro() {
+int compraCarro() {
+	system("cls");
 	
+	puts("1 - Siena");
+	puts("2 - Civic");
+	puts("3 - Lancer\n");
+	puts("escolha uma opção:");
+	
+	fflush(stdin);
+	scanf("%d", &optionCar);
+	fflush(stdin);
+	
+	switch(optionCar) {
+		case 1:
+			carName = "Siena";
+			break;
+			
+		case 2:
+			carName = "Civic";
+			break;
+			
+		case 3:
+			carName = "Lancer";
+			break;
+			
+		default:
+			system("cls");
+			puts("numero inválido");
+			system("pause");
+			compraCarro();
+			break;
+	}
+printf("Carro: %s\n", carName);
+	
+	puts("Qual a cor do seu carro?");
+	fflush(stdin);
+	gets(cor);
+	
+	puts("qual a forma de pagamento?");
+	fflush(stdin);
+	gets(pagamento);
+	
+	system("cls");
+	
+	printf("Carro: %s\n", carName);
+	printf("Cor do carro: %s\n", cor);
+	printf("Forma de pagamento: %s\n\n", pagamento);
+	
+	system("pause");
+	
+	menu();
 }
 
 int compraAcessorio() {
+	system("cls");
 	
-}*/
+	puts("ta rodando 2");
+	
+	system("pause");
+	
+	menu();
+}
+
+int sair() {
+	system("cls");
+	
+	puts("Programa Finalizado!");
+}
 
 int main() {
 	setlocale(LC_ALL, "Portuguese");
 	
 	login();
-	cadastro();
+	
+	menu();
+	
 }
 
 
